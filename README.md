0154e487b705b59fe26e3b9c179174cca1b498ae
jojomorocho <ljoelemanuel@gmail.com>
2021-11-30 08:28:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fk9l8k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fk9l8k)
---
+++ src/Proyectos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './style';
+
+const Proyectos = () => {
+  return (
+    <>
+    <Navbar />
+    
+    </>
+  );
+};
+
+export default Proyectos;
